5bf25a4e0adf16294fa944b2b8894618012b19ae
EJSohn <5on.3un.joo@gmail.com>
2016-08-22 17:40:53 +0900

    빈 로커 반환 함수 작성 및 서버 api get 테스트 완료

--- server/hybrary.py
+++ server/hybrary.py
@@ -1,11 +1,23 @@
+# -*- coding: utf-8 -*-
+# coded by EJSohn
+
 from flask import Flask
+from flask import jsonify
+from seat_finder import returnEmptyLoackers, returnReadingRoomInfo
+
 application = Flask(__name__)
 
 
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>in changes?</h1>"
+    return "<h1 style='color:blue'>Test page.</h1>"
+
+@application.route("/v1.0/getSeoulEmptyLockers")
+def getSeoulEmptyLockers():
+    returnval = returnEmptyLoackers()
+
+    return jsonify(returnval)
 
 
 if __name__ == "__main__":
