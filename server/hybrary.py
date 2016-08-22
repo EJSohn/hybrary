8559ed80d6e97de8712f0402e67437baaf96ca0a
EJSohn <5on.3un.joo@gmail.com>
2016-08-22 17:50:31 +0900

    백남/법학 도서관 열람실 좌석 갯수 반환 함수 작성 및 api get 테스트 완료

--- server/hybrary.py
+++ server/hybrary.py
@@ -13,12 +13,20 @@ application = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Test page.</h1>"
 
+
+# 서울캠퍼스 전자사물함의 총 사물함 수, 빈 사물함 수, 업데이트 일시분초를 json으로 반환해주는 함수
 @application.route("/v1.0/getSeoulEmptyLockers")
 def getSeoulEmptyLockers():
     returnval = returnEmptyLoackers()
 
     return jsonify(returnval)
 
+# 서울캠퍼스 각 열람실의 열람실이름, 총 자리 갯수, 빈 자리의 갯수를 json으로 반환해주는 함수
+@application.route("v1.0/getSeoulEmptyReadingRoom")
+def getSeoulEmptyReadingRoom():
+    returnval = returnReadingRoomInfo()
+
+    return jsonify(returnval)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port='60')
