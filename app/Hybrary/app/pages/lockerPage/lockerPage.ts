e324c9c3f13007d6e3d59f72c6aa3b63d0475d6d
EJSohn <5on.3un.joo@gmail.com>
2016-08-26 03:13:55 +0900

    complete lockers update page.

--- app/Hybrary/app/pages/lockerPage/lockerPage.ts
+++ app/Hybrary/app/pages/lockerPage/lockerPage.ts
@@ -12,13 +12,13 @@ export class LockerPage {
 
     // local variables
     nav: any;
-    lockersInfo: any;
+    lockers: any;
 
     constructor(nav, navParams){
         this.nav = nav;
 
         // get lockers Info
-        this.lockersInfo = navParams.get("lockersInfo");
+        this.lockers = navParams.get("lockersInfo");
     }
 
     alertOn(event){
@@ -26,5 +26,41 @@ export class LockerPage {
         $("#alertButton").addClass("redClass");
 
         $("#alertText").text("");
+    }
+
+    dataUpdate(event){
+        // update data 
+
+        var nav = this.nav;
+        var loading = Loading.create({
+            content: "Loading...",
+            dismissOnPageChange: true
+        });
+        this.nav.present(loading);
+
+        $.ajax({
+            url: "http://hyuis.kr:60/v1.0/getSeoulEmptyLockers",
+            async: true,
+            dataType: "json",
+            success: function(data){
+                // data receive success
+                LockerPage.prototype.lockers = data;
+                loading.dismiss();
+            },
+            error: function(){
+                // warning
+                loading.dismiss();
+
+                let alert = Alert.create({
+                    title: "문제가 발생하였습니다.",
+                    subTitle: "네트워크를 확인하고 다시 한 번 시도해주세요.",
+                    buttons: ["OK"]
+                });
+                nav.preset(alert);
+            }
+        }).then(()=>{
+            this.lockers = LockerPage.prototype.lockers;
+            loading.dismiss();
+        });
     }
 }
