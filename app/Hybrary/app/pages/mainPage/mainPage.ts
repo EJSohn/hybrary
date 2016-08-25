6652814f57a8d243d6f23bad8458952011ff8918
EJSohn <5on.3un.joo@gmail.com>
2016-08-25 17:18:13 +0900

    create connection between mainPage and lockerPage

--- app/Hybrary/app/pages/mainPage/mainPage.ts
+++ app/Hybrary/app/pages/mainPage/mainPage.ts
@@ -1,6 +1,9 @@
 import {NavController, NavParams, Storage, LocalStorage, SqlStorage, Alert, Loading} from 'ionic-angular';
 import {Component} from "@angular/core";
 
+// import components
+import {LockerPage} from '../lockerPage/lockerPage';
+
 @Component({
     templateUrl: 'build/pages/mainPage/mainPage.html'
 })
@@ -10,7 +13,16 @@ export class MainPage {
         return [[NavController], [NavParams]]
     }
 
+    // local variables.
+    nav: any;
+
     constructor(nav, navParams){
-        
+        this.nav = nav;
     }
+
+    moveLockerPage(event){
+        // move to locker page.
+        this.nav.push(LockerPage);
+    }
+
 }
