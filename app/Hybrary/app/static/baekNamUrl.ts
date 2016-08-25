c3a3cdd2838d87736cc4cea0829272524423815b
EJSohn <5on.3un.joo@gmail.com>
2016-08-26 04:09:59 +0900

    create reading room seats view url file and iframe page that show that url.

---
+++ app/Hybrary/app/static/baekNamUrl.ts
@@ -0,0 +1,9 @@
+var host_url = "http://166.104.182.141/seats/roomview5.asp?";
+
+export var urlInfo = {
+    "제1열람실 3층": host_url+"room_no=1",
+    "제2열람실A 4층": host_url+"room_no=2",
+    "제2열람실B 4층	": host_url+"room_no=3",
+    "대학원열람실 2층": host_url+"room_no=4"
+};
+
