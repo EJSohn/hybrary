44473c9bab8b7fba30df8b3e35878b739aaf0101
EJSohn <5on.3un.joo@gmail.com>
2016-08-28 15:24:07 +0900

    fix header and use url safer

--- app/Hybrary/app/pages/iframePage/iframe.ts
+++ app/Hybrary/app/pages/iframePage/iframe.ts
@@ -1,5 +1,6 @@
 import {NavController, NavParams, Storage, LocalStorage, SqlStorage, Alert ,Loading} from 'ionic-angular';
 import {Component} from "@angular/core";
+import { SafeResourceUrl, DomSanitizationService } from '@angular/platform-browser';
 
 @Component({
     templateUrl: 'build/pages/iframePage/iframe.html'
@@ -7,17 +8,17 @@ import {Component} from "@angular/core";
 
 export class IframePage {
     static get parameters(){
-        return [[NavController], [NavParams]]
+        return [[NavController], [NavParams], [DomSanitizationService]]
     }
 
     // local variables
     nav: any;
-    Src: any;
+    Src: SafeResourceUrl;
     RRname: any;
 
-    constructor(nav, navParams){
+    constructor(nav, navParams, sanitizer){
         this.nav = nav;
-        this.Src = navParams.get('showSrc');
+        this.Src = sanitizer.bypassSecurityTrustResourceUrl(navParams.get('showSrc'));
         this.RRname = navParams.get('RRname');
 
         console.log(this.Src);
